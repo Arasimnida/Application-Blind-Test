2a4088a8a6f35c33570d224fda5ea3699686fce6
Arasimnida <88892132+Arasimnida@users.noreply.github.com>
2023-04-11 21:19:50 +0200

    feat :titre 1ere musique ; nb de musique; recolter les musiques depuis spotify; changer de musique

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
+
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+
+  const {
+    data: tracks,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+
+  console.log(tracks);
+  if (isError) return <div>Error</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  const name0 = tracks?.[0];
+  const goToNextTrack = () => {
+    setTrackIndex((trackIndex + 1) % tracks.length);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +41,20 @@ const App = () => {
       </header>
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
-        <p>test</p>
+        <p>Il y a actuellement {tracks.length} titres dans le blind test</p>
+        {name0 ? (
+          <p>Titre de la première musique : {name0.track.name}</p>
+        ) : (
+          <div>No tracks</div>
+        )}
+        {tracks && (
+          <audio
+            src={tracks?.[trackIndex]?.track.preview_url}
+            autoPlay
+            controls
+          />
+        )}
+        <button onClick={goToNextTrack}>Next track</button>
       </div>
       <div className="App-buttons"></div>
     </div>
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { SavedTrack } from 'spotify-types';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+const apiToken =
+  'BQAUWzC5alKfe1v1YUV9jpeq8DxFG4whq5BpHmMziqaM919ni97V3_T13hYr_yzICnx-N1HoC_2aQQDbS8iDXXlw0IxeNiqySOVUr0YGeY7Tkyp0qig8CDm7RoR7nHWXbg0L7tNHn7YDErTzdT6tQ38HSBlNsgAeIwOyXmUMhYqkpUhoXZQHZbz7zYUyCrFq9w5-DZUihN4ziujj-_xHfzzJfyOgt7r1UhlV1v60AUxrhoAptY9QZ5D82HWJbPYQVj9qpGUrRIHhM54ADNMD1jQQuhchcaFGHRkpiTLpvuJj6mWpUDUUI7bcovY6dDELMqo_v-buW0L1TdtQo9-pGTU3TssLiojaTouLpMWMN8LWtGE';
+
+export const fetchTracks: () => Promise<Array<SavedTrack>> = async () => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: SavedTrack[] };
+
+  return data.items;
 };
