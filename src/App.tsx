02f7b0f7edfa2fb7cd1d8adaa1f06eb48616c8e9
Arasimnida <88892132+Arasimnida@users.noreply.github.com>
2023-04-18 10:39:43 +0200

    enlever couverture album

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,6 @@ import { fetchTracks } from './lib/fetchTracks';
 import { useQuery } from '@tanstack/react-query';
 import { SavedTrack } from 'spotify-types';
 
-const trackUrls = [
-  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
-  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
-  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
-  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
-  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
-];
-
 const AlbumCover = (track: SavedTrack) => {
   const src = track.track.album.images[0]?.url;
   return <img src={src} style={{ width: 400, height: 400 }} />;
@@ -58,7 +50,6 @@ const App = () => {
         {currentTrack && (
           <>
             <audio src={currentTrack.track.preview_url} autoPlay controls />
-            <AlbumCover track={currentTrack} />
           </>
         )}
         <button onClick={goToNextTrack}>Next track</button>
